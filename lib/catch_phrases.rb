83999f67578845a0313456bfded14ecd80986d23
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-06 17:28:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,12 @@
+<<<<<<< HEAD
 
 def mario(phrase ="It's-a me, Mario!")
+=======
+def mario(phrase ="It's-a me, Mario!")
+status = "Thank You Mario! But Our Princess Is In Another Castle!"
+>>>>>>> be1f977bf4f7a2323903bdaa516e9d6004f9ad66
   puts phrase
+  
 end
 
 def toadstool(status= "Thank You Mario! But Our Princess Is In Another Castle!")
